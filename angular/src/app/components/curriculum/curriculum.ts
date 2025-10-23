6b758a992f3f0f855ba8ddf6ded80d690d8a4b68
leomorabito02 <leonardomorabito02@gmail.com>
2025-10-22 23:14:06 -0300

    arreglo de fotos de perfil

--- angular/src/app/components/curriculum/curriculum.ts
+++ angular/src/app/components/curriculum/curriculum.ts
@@ -13,7 +13,6 @@ export class Curriculum {
   Student1 = {
     name: 'Leonardo Morabito',
     // Ruta corregida: de 'image' a 'images'
-    avatar : '../../assets/images/leo.jpg', 
     role: 'Estudiante de Ingeniería en Sistemas',
     email: 'leonardomorabito02@gmail.com',
     tel: '+54 9 351 394 1076',
@@ -23,7 +22,7 @@ export class Curriculum {
   Student2 = {
     name: 'Victoria Elliott',
     // Esta ruta ya estaba correcta
-    avatar: '../../assets/images/miche.jpeg',
+    avatar: 'assets/images/miche.jpeg',
     role: 'Estudiante de Ingeniería en Sistemas',
     email: 'vikielliott30@gmail.com',
     tel: '+54 9 3521 43-9947',
